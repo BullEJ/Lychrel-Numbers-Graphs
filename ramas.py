9c2c12139d53d676be2e37492207d8f61a55cd35
BullEJ <emmanuel.1502.jt@gmail.com>
2024-12-18 20:54:55 -0300

    Progreso en T1ln9. Y las tuplas se recorren más rápido que los sets.

--- ramas.py
+++ ramas.py
@@ -1,20 +1,20 @@
-from networkx import DiGraph, is_isomorphic, diameter                         #
+from networkx import DiGraph, is_isomorphic
 from utilities import antecesores, falta_completar
 class Rama(DiGraph):
     def __init__(self, l1: tuple):
         super().__init__()
         self.fri : tuple[int] = l1
         
-        filter_seeds = falta_completar({l1})
+        filter_seeds = falta_completar((l1, ))
         while filter_seeds:
             for nodo in filter_seeds:
                 self.add_edges_from(map(
                     lambda ant: (ant, nodo), # (antecesor, sucesor)
                     antecesores(nodo)))
 
-            filter_seeds = falta_completar(list(map(
+            filter_seeds = falta_completar(map(
                             lambda nodo: self.predecessors(nodo),
-                            filter_seeds)))
+                            filter_seeds))
 
     def __eq__(self, other):
         return is_isomorphic(self, other)
--- utilities.py
+++ utilities.py
@@ -7,14 +7,16 @@ import shutil
 from os.path import join, exists
 from os import remove
 from functools import reduce
-from itertools import product
+from itertools import product, combinations, dropwhile, takewhile
+
+import time
 
 # Para ramas.py
     # Condicionadores de existencia
-def blq(l: list, i: int, c_pos = True): # c_pos = condición de positividad
+def blq(t: tuple, i: int, c_pos = True): # c_pos = condición de positividad
     try:
         if c_pos and 0 > i: raise IndexError
-        return l[i]
+        return t[i]
     except IndexError: return 0
 
 def is_seed_t1l9(fri):
@@ -29,47 +31,76 @@ def is_seed_t2l9(fri):
 def is_seed_t2ln9(fri):
     pass
 
-def is_seed(fri: list[int]) -> bool:
+def is_seed(fri: tuple[int]) -> bool:
     return (is_seed_t1l9(fri) or is_seed_t1ln9(fri) or
             is_seed_t2l9(fri) or is_seed_t2ln9(fri))
 
     # Algoritmos para obtener antecesores.
     # Acá se presupone que los fri_s entregados cumplen las condiciones para
     # poseer antecesor de cierto tipo.
-def T1l9(fri_s: list[int], paridad: int) -> list[list]:
+def T1l9(fri_s: tuple[int], paridad: int):
     C  = [0] + reduce(
-                lambda c, i: c + [(blq(c, -2, False) + i) % 2],
-                fri_s[:-1], [])
-    MC = [blq(C, i-1) + blq(C, i+1) for i in range(len(C))]
-    MC = MC[:-1] + [MC[-1] + C[-1 - paridad]]
+                lambda c, i: c + ((blq(c, -2, False) + i) % 2, ),
+                fri_s[:-1], tuple())
+    MC = (blq(C, i-1) + blq(C, i+1) for i in range(len(C)))
+    MC = MC[:-1] + tuple(MC[-1] + blq(C, -1 - paridad, False))
     
-    yield [int(0.5 * (fri_s[i] - MC[i])) + 10 * C[i]
-             for i in range(len(C))]
+    yield tuple(int(0.5 * (fri_s[i] - MC[i])) + 10 * C[i]
+             for i in range(len(C)))
 
-def T1ln9(fri: list[int]) -> list[list]:
-    pass
+def T1ln9(fri_s: tuple[int]):#, paridad: int):
+    
+    def all_combinations():
+        # Extraer posiciones nueves.
+        secuencias_nueves = []
+        secuencia_actual  = []
+        
+            # Iteramos sobre la fri_s
+        for indice, num in enumerate(fri_s[1:-1], 1):
+            if num == 9:
+                secuencia_actual.append(indice)
+            else:
+                if secuencia_actual: 
+                    secuencias_nueves.append(tuple(secuencia_actual))
+                    secuencia_actual = [] 
+        
+            # Si al final de la iteración aún hay una secuencia de 9's
+        if secuencia_actual:
+            secuencias_nueves.append(tuple(secuencia_actual))
 
-def T2l9(fri_s: list[int], paridad: int) -> list[list]:
+        for r in range(1, len(secuencias_nueves) + 1):
+            yield from combinations(secuencias_nueves, r)
+
+    def remover_bloques(set_bloques):
+        for bloque in set_bloques:
+            if len(bloque) > 2:
+                return tuple()
+
+    for set_blqs in all_combinations():
+
+
+def T2l9(fri_s: tuple[int], paridad: int):
     BS = reduce(
-            lambda c, i: c + [i - blq(c, -1, False)],
-            fri_s, [])
+            lambda c, i: c + tuple(i - blq(c, -1, False), ),
+            fri_s, tuple())
 
     for infc in product({0, 1}, repeat = len(fri_s) - 1 - paridad):
-        yield [BS[i] + 9 * ((1,) + infc)[i] for i in range(len(infc) + 1)]
+        yield tuple(BS[i] + 9 * ((1,) + infc)[i] for i in range(len(infc) + 1))
 
-def T2ln9(fri: list[int]) -> list[list]:
+def T2ln9(fri: tuple[int]):
     pass
 
-def antecesores(fri: list[int], paridad):
+def antecesores(fri: tuple[int], paridad):
     yield from T1l9(fri, paridad)
     yield from T1ln9(fri, paridad)
     yield from T2l9(fri, paridad)
     yield from T2ln9(fri, paridad)
 
     #Miscelaneos
-def falta_completar(nodos: set[tuple]) -> set[tuple]:
+def falta_completar(nodos) -> tuple[tuple]:
     # Retorna los FRI que no son semilla
-    return set(filter(
+    # nodos es un generador
+    return tuple(filter(
                     lambda nodo: not is_seed(nodo),
                     nodos))
 
